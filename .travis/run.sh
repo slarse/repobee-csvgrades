02aa2d8148a6619129304a164a57aac1eb09beff
Simon Larsén <slarse@kth.se>
2019-08-27 22:45:02 +0200

    [misc] Fix misconfigured Travis builds (#6)
    Now they fail because the tests/checks fail, not because the builds are misconfigured!

---
+++ .travis/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function run_flake8() {
+    pip install flake8
+    flake8 .
+    if [[ ! $? == 0 ]]; then
+        exit 1
+    fi
+}
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    eval "$(pyenv init -)"
+    pyenv local 3.5.4 3.6.5 3.7.0
+    pyenv global 3.7.0
+    run_flake8
+    tox
+else
+    run_flake8
+    pytest tests --cov=repobee_csvgrades --cov-branch
+fi
